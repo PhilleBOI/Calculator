38ceb1a899893f5ef8bc380cd2166ee8a90e2624
PhilleBOI <54800654+PhilleBOI@users.noreply.github.com>
2020-11-27 15:47:54 +0100

    11-27

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -25,60 +25,121 @@ namespace Calculator
         {
             InitializeComponent();
         }
-
         public void Btn_nummer(Object sender, RoutedEventArgs e)
         {
-
+            if (e.Source is Button button)
+            {
+                TextField.Text += button.Content;
+            }
         }
 
         public void Btn_procent(Object sender, RoutedEventArgs e)
         {
-            
+            if (e.Source is Button button)
+            {
+                TextField.Text += button.Content;
+            }
         }
 
         public void Btn_CE(Object sender, RoutedEventArgs e)
         {
-
+            if (e.Source is Button button)
+            {
+                
+            }
         }
 
         public void Btn_C(Object sender, RoutedEventArgs e)
         {
-
+            if (e.Source is Button button)
+            {
+                TextField.Text = "";
+            }
         }
 
         public void Btn_Delat(Object sender, RoutedEventArgs e)
         {
-
+            if (e.Source is Button button)
+            {
+                TextField.Text += button.Content;
+            }
         }
 
         public void Btn_gånger(Object sender, RoutedEventArgs e)
         {
-
+            if (e.Source is Button button)
+            {
+                TextField.Text += button.Content;
+            }
         }
 
         public void Btn_minus(Object sender, RoutedEventArgs e)
         {
-
+            if (e.Source is Button button)
+            {
+                TextField.Text += button.Content;
+            }
         }
 
         public void Btn_plus(Object sender, RoutedEventArgs e)
         {
-
+            if (e.Source is Button button)
+            {
+                TextField.Text += button.Content;
+            }
         }
 
         public void Btn_likamed(Object sender, RoutedEventArgs e)
         {
-
+            if (e.Source is Button button)
+            {
+                TextField.Text += button.Content;
+            }
         }
 
         public void Btn_comma(Object sender, RoutedEventArgs e)
         {
-
+            if (e.Source is Button button)
+            {
+                TextField.Text += button.Content;
+            }
         }
 
         public void Btn_calc(Object sender, RoutedEventArgs e)
         {
+            var numbers = TextField.Text.Split('+', '-', '÷', '*');
 
+            if (e.Source is Button button)
+            {
+                if(TextField.Text.Contains('+')) {
+                var sum = Convert.ToDouble(numbers[0]) + Convert.ToDouble(numbers[1]);
+                TextField.Text = sum + "";
+                }
+
+                if (TextField.Text.Contains('-'))
+                {
+                    var sum = Convert.ToDouble(numbers[0]) - Convert.ToDouble(numbers[1]);
+                    TextField.Text = sum + "";
+                }
+
+                if (TextField.Text.Contains('÷'))
+                {
+                    var sum = Convert.ToDouble(numbers[0]) / Convert.ToDouble(numbers[1]);
+                    TextField.Text = sum + "";
+                }
+
+                if (TextField.Text.Contains('*'))
+                {
+                    var sum = Convert.ToDouble(numbers[0]) * Convert.ToDouble(numbers[1]);
+                    TextField.Text = sum + "";
+                }
+
+                if (TextField.Text.Contains('%'))
+                {
+                    var sum = Convert.ToDouble(numbers[0]) % Convert.ToDouble(numbers[1]);
+                    TextField.Text = sum + "";
+                }
+            }
         }
     }
 }
